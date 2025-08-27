48e9a464cfe24dd8400044cf12d1353a43416087
Gowthami077 <137594225+Gowthami1214@users.noreply.github.com>
2025-08-27 13:37:27 +0530

    [496] [Time Beats: 91.62%] [Memory Beats: 40.85%] - LeetPush

--- Next-Greater-Element-I.java
+++ Next-Greater-Element-I.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> ngeMap=new HashMap<>();
-       // int[] res=new int[int1.length];
+        HashMap<Integer,Integer> map=new HashMap<>();
         Stack<Integer> stack=new Stack<>();
-        for(int i=nums2.length-1;i>=0;i--){
-            int num=nums2[i];
-            while(!stack.isEmpty() && stack.peek()<=num){
-                stack.pop();
+        for(int num:nums2){
+            while(!stack.isEmpty() && num>stack.peek()){
+                map.put(stack.pop(),num);
             }
-            ngeMap.put(num,stack.isEmpty()?-1 : stack.peek());
             stack.push(num);
         }
-        int[] res=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            res[i]=ngeMap.get(nums1[i]);
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
         }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
+        }
+
         return res;
     }
 }
