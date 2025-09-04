6af6c38c06cb7797433754155e26f648907aa07c
Gowthami077 <137594225+Gowthami1214@users.noreply.github.com>
2025-09-04 21:03:12 +0530

    [1081] [Time Beats: 66.86%] [Memory Beats: 5.44%] - LeetPush

---
+++ Smallest-Subsequence-of-Distinct-Characters.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        Stack<Character> stack = new Stack<>();
+        boolean[] visited = new boolean[26];
+        int[] lastIndex = new int[26];
+
+        
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (visited[c - 'a']) continue;
+
+            while (!stack.isEmpty() && stack.peek() > c && i < lastIndex[stack.peek() - 'a']) {
+                char popped = stack.pop();
+                visited[popped - 'a'] = false;
+            }
+
+            stack.push(c);
+            visited[c - 'a'] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
