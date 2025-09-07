7b3452c4048b3e4006782e52e2d52dcf4fe8be1e
Gowthami077 <137594225+Gowthami1214@users.noreply.github.com>
2025-09-07 20:08:13 +0530

    [154] [Time Beats: 100.00%] [Memory Beats: 87.70%] - LeetPush

---
+++ Find-Minimum-in-Rotated-Sorted-Array-II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            return nums[0];
+        }
+        if(nums[0]<nums[nums.length-1]){
+            return nums[0];
+        }
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<=nums[right]){
+                right--;;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        return nums[left];
+    }
+}
