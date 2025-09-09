3b3ca10b1c3b519d505fad19628bcdb31756f087
Gowthami077 <137594225+Gowthami1214@users.noreply.github.com>
2025-09-09 20:54:02 +0530

    [707] [Time Beats: 98.54%] [Memory Beats: 92.86%] - LeetPush

---
+++ Design-Linked-List.java
@@ -0,0 +1,36 @@
+class MyLinkedList {
+    LinkedList<Integer> list;
+
+    public MyLinkedList() {
+        list = new LinkedList<>();
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= list.size()) {
+            return -1; 
+        }
+        return list.get(index);
+    }
+
+    public void addAtHead(int val) {
+        list.addFirst(val);
+    }
+
+    public void addAtTail(int val) {
+        list.addLast(val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > list.size()) {
+            return; 
+        }
+        list.add(index, val);
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= list.size()) {
+            return; 
+        }
+        list.remove(index);
+    }
+}
