d77948db1f566c1d1ab26bd9c936623623cb2b46
Gowthami077 <137594225+Gowthami1214@users.noreply.github.com>
2025-10-27 16:49:45 +0530

    [121] [Time Beats: 73.91%] [Memory Beats: 31.28%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.java
+++ Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,11 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-     int min=Integer.MAX_VALUE;
-     int max=Integer.MIN_VALUE;
-     for(int i=0;i<prices.length;i++){
-        min=Math.min(prices[i],min);
-        max=Math.max(max,prices[i]-min);
-     }   
-     return max;
+        int max=0;
+        int min=prices[0];
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else
+            {
+                int current=prices[i]-min;
+                max=Math.max(current,max);
+            }
+        }
+        return max;
     }
 }
